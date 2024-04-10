6eb8453d7b694941bfc60ef9ed0e26a8e85d85d0
nszul44 <136347369+nszul44@users.noreply.github.com>
2024-04-09 21:32:26 -0400

    myHeap

--- myHeap.cpp
+++ myHeap.cpp
@@ -5,18 +5,18 @@ void heapMedian(const std::vector<int> * instructions){
     std::priority_queue<int, std::vector<int>, std::greater<int>> large;
     std::vector<int> output;
     for(auto it = instructions->begin(); it != instructions->end(); ++it){
-        if(small.size() > large.size() + 1){  //rebalance heaps
+        if(small.size() > large.size() + 1){  //rebalance heaps if small is too big
                 large.push(small.top());
                 small.pop();
             } 
-        else if (large.size() > small.size()) {
+        else if (large.size() > small.size()) { //rebalance heaps if large is too big
             small.push(large.top());
             large.pop();
         }
         if(*it == -1){
             output.push_back(small.top());
             small.pop();
-            if(small.size() == (large.size() - 1)){  //rebalance heaps
+            if(small.size() == (large.size() - 1)){  //rebalance heaps if small is too big after popping the median
                 small.push(large.top());
                 large.pop();
             }
